1ea0c21373eae7b95b4f07325cbedffb3aa58397
kandunuri-manisha <manishakandunuri@gmail.com>
2025-12-30 10:35:58 +0530

    Add functions for submitting data and viewing products

---
+++ script.js
@@ -0,0 +1,16 @@
+function submitFarmerData() {
+    alert("Farmer data submitted successfully!");
+}
+
+function submitDistributorData() {
+    alert("Distributor update successful!");
+}
+
+function submitRetailerData() {
+    alert("Retailer confirmation successful!");
+}
+
+function viewProduct() {
+    document.getElementById("result").innerHTML =
+        "<p>Product details will be displayed here.</p>";
+}
